5f0abd89355d1dc10e1b8d4117bea9e8e53c0e1a
Vitor Zanelli <107404266+vitorzanelli07@users.noreply.github.com>
2024-04-16 11:50:45 -0300

    hill

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("Qual foi o único piloto a conquistar a Triplice Coroa do Automobilismo ?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "Graham Hill ") {
+      alert("Isso mesmo! Graham Hill foi o único piloto da historia a conquistar a Triplice Coroa");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
